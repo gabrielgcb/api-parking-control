4bf01909b9f0afc36c9b9fbf533dfc9df084cc50
Gabriel Bispo <gabriel.bispo@discente.ufg.br>
2023-09-21 14:30:02 -0300

    parking-control/feat: service

---
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,54 @@
+package com.api.parkingcontrol.services;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.logging.Logger;
+
+@Service
+public class ParkingSpotService {
+
+    private final Logger logger = Logger.getLogger(ParkingSpotService.class.getName());
+
+    @Autowired
+    private ParkingSpotRepository parkingSpotRepository;
+
+    @Transactional
+    public Object save(ParkingSpotModel parkingSpotModel) {
+        logger.info("Creating a new parking information!");
+        return parkingSpotRepository.save(parkingSpotModel);
+    }
+
+    public boolean existsByPlateLicenseCar(String licensePlateCar) {
+        return parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
+    }
+
+    public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+        return parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
+    }
+
+    public boolean existsByApartmentAndBlock(String apartment, String block) {
+        return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
+    }
+
+    public Page<ParkingSpotModel> findAll(Pageable pageable) {
+        return parkingSpotRepository.findAll(pageable);
+    }
+
+    public Optional<ParkingSpotModel> findById(UUID id) {
+        return parkingSpotRepository.findById(id);
+    }
+
+    @Transactional
+    public void delete(ParkingSpotModel parkingSpotModel) {
+        parkingSpotRepository.delete(parkingSpotModel);
+    }
+}
